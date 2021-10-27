e1ccda542839704253b01b9865ee4b4aeeefb3e8
Nelson David Daza <yankeenelson@gmail.com>
2021-10-27 12:31:56 -0700

    Create An Entry Tasks 1-12

--- js/main.js
+++ js/main.js
@@ -1,2 +1,39 @@
+/* eslint-disable no-unused-vars */
 /* global data */
 /* exported data */
+var $photoUrl = document.querySelector('.photo-url');
+var $img = document.querySelector('img');
+var $entryForm = document.querySelector('.entry-form');
+var $comments = document.querySelector('.comments');
+var $photoTitle = document.querySelector('.photo-title');
+var $form = document.querySelector('form');
+
+function srcUpdate(event) {
+  $img.setAttribute('src', $photoUrl.value);
+
+}
+
+$photoUrl.addEventListener('input', srcUpdate);
+
+var submissionDataModel = [];
+
+function submitFunction(event) {
+  event.preventDefault();
+  var photoTitleValue = $form.elements.photoTitle.value;
+  var commentsValue = $form.elements.comments.value;
+  var photoUrlValue = $form.elements.photoUrl.value;
+  var submissionObject = { photoTitleValue, photoUrlValue, commentsValue };
+  submissionDataModel.push(submissionObject);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+
+}
+
+$entryForm.addEventListener('submit', submitFunction);
+
+function beforeUnload(event) {
+  var submissionDataModelJSON = JSON.stringify(submissionDataModel);
+  localStorage.setItem('javascript-local-storage', submissionDataModelJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnload);
